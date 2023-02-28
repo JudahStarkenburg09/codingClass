57c1990c303550dd6ef3decb3a1aa72bf6153f91
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-02-28 14:34:02 -0600

    dfdsf

--- chatbot-project/carHighway.py
+++ chatbot-project/carHighway.py
@@ -45,32 +45,32 @@ carSpeedingSpeed = 0
 carIsRotated = False
 roadSpeed = 3.5
 spark = pygame.image.load('sparks.png')
-spark = pygame.transform.scale(spark, [100, 100])
+spark = pygame.transform.scale(spark, [40, 50])
 
 obstacle1 = pygame.image.load('obstacleCar1.png')
 obstacle1 = pygame.transform.scale(obstacle1, [70, 120])
 obstacle1 = pygame.transform.rotate(obstacle1, 0)
-obstacle1R = obstacle1.get_rect()
+obstacle1 = obstacle1.get_rect()
 
 obstacle2 = pygame.image.load('obstacleCar2.png')
 obstacle2 = pygame.transform.scale(obstacle2, [70, 120])
 obstacle2 = pygame.transform.rotate(obstacle2, -90)
-obstacle2R = obstacle2.get_rect()
+obstacle2 = obstacle2.get_rect()
 
 obstacle3 = pygame.image.load('obstacleCar3.png')
 obstacle3 = pygame.transform.scale(obstacle3, [70, 120])
 obstacle3 = pygame.transform.rotate(obstacle3, 0)
-obstacle3R = obstacle3.get_rect()
+obstacle3 = obstacle3.get_rect()
 
 obstacle4 = pygame.image.load('obstacleCar4.png')
 obstacle4 = pygame.transform.scale(obstacle4, [70, 120])
 obstacle4 = pygame.transform.rotate(obstacle4, 0)
-obstacle4R = obstacle4.get_rect()
+obstacle4 = obstacle4.get_rect()
 
 obstacle5 = pygame.image.load('obstacleCar5.png')
 obstacle5 = pygame.transform.scale(obstacle5, [70, 120])
 obstacle5 = pygame.transform.rotate(obstacle5, 90)
-obstacle5R = obstacle5.get_rect()
+obstacle5 = obstacle5.get_rect()
 
 choiceOfObstacleOnScreen1 = random.choice([10,100,190])
 
@@ -87,7 +87,13 @@ while True:
             sys.exit()
 
 
-    
+
+    screen.blit(roadImage, (0, roadPos - SCREEN_HEIGHT))
+    screen.blit(roadImage, (0, roadPos))
+
+    screen.blit(car, (carPosx, carPosy))
+
+
 
     # Scroll the road image
     roadPos += roadSpeed
@@ -113,8 +119,19 @@ while True:
             carXspeed -= .01
         carXspeed += .01
     if keys[pygame.K_LEFT]:
+        if carIsRotated != True:
+            car = pygame.image.load('carTop.png')
+            car = pygame.transform.scale(car, [70, 120])
+            car = pygame.transform.rotate(car, 4)
+            carIsRotated = True
         carPosx -= carXspeed
-    if keys[pygame.K_RIGHT]:
+    
+    elif keys[pygame.K_RIGHT]:
+        if carIsRotated != True:
+            car = pygame.image.load('carTop.png')
+            car = pygame.transform.scale(car, [70, 120])
+            car = pygame.transform.rotate(car, -4)
+            carIsRotated = True
         carPosx += carXspeed
     elif carIsRotated is True:
         carIsRotated = False
@@ -124,19 +141,17 @@ while True:
 
 
     if carPosx < 10:
+        screen.blit(spark, (carPosx - 20, carPosy - 20))
         carPosx += carXspeed
 
 
     if carPosx > 185:
-        screen.blit(spark, (carPosx + 20, carPosy - 10))
+        
+        screen.blit(spark, (carPosx + 40, carPosy - 20))
         carPosx -= carXspeed
 
 
     # Draw the road image
-    screen.blit(roadImage, (0, roadPos - SCREEN_HEIGHT))
-    screen.blit(roadImage, (0, roadPos))
-
-    screen.blit(car, (carPosx, carPosy))
 
     
 
