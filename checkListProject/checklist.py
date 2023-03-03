82f540035d2a86c53f966ca4aaf9a065aaeb9791
judahstarkenburg09 <judahstarkenburg@gmail.com>
2023-03-03 11:21:52 -0600

    commiting, having problem could be with school

--- checkListProject/checklist.py
+++ checkListProject/checklist.py
@@ -1,14 +1,13 @@
 import gspread
 from google.oauth2.service_account import ServiceAccountCredentials
 
-
 # set up credentials
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name('your_credentials.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name('sodium-lodge-376703-8f5ab782026c.json', scope)
 client = gspread.authorize(creds)
 
 # open the Google Sheets document by its URL
-sheet_url = 'https://docs.google.com/spreadsheets/d/your_sheet_id/edit#gid=0'
+sheet_url = 'https://docs.google.com/spreadsheets/d/1Bl0qatKOkC2_boYVE3W0VW01COOucE777mlZywRwg7E/edit#gid=0'
 sheet = client.open_by_url(sheet_url)
 
 # select a worksheet by its index (starting from 0)
