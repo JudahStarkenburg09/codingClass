3326226e8e4e735894a20fa1c28e4240fe5bd111
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-03-07 19:26:12 -0600

    captcha done

--- assignments/captcha.py
+++ assignments/captcha.py
@@ -1,45 +1,32 @@
 from tkinter import *
 import random
+import time
+wronged = False
 
-window = Tk()
-window.geometry('300x300')
-
-canvas = Canvas(window, width=250, height=250, bg="white")
-canvas.pack()
-
-# create the square shape
-square = canvas.create_rectangle(60, 60, 100, 100, fill="white")
-
-# define the checkmark line coordinates
-checkmark_coords = [(70, 80), (80, 90), (95, 70)]
-
-# create a boolean variable to keep track of whether the checkbox is checked
-checked = False
-
-# create a function to handle checkbox clicks
-def on_checkbox_click(event):
-    global checked
-    if not checked:
-        # draw the checkmark line on top of the square
-        canvas.create_line(*checkmark_coords, width=3, fill="black")
-        checked = True
-    else:
-        # delete the checkmark line
-        canvas.delete("all")
-        canvas.create_rectangle(60, 60, 100, 100, fill="white", width=2)
-        checked = False
-
-# bind the click event to the square
-canvas.tag_bind(square, "<Button-1>", on_checkbox_click)
-
-window.mainloop()
+notRobot = False
 
 
+def runNextFunction():
+    #COMPLETE CAPTCHA HERE:
+    
+    tk = Tk()
+    tk.title('Success')
+    tk.geometry('300x50')
+    success = Label(tk, text="You Are Not A Robot")
+    success.pack()
+    tk.mainloop()
+
+def on_window_close():
+    global notRobot
+    global wronged
+    notRobot = True
+    window.destroy()
 
 
 def captcha():
     window.destroy()
     root = Tk()
+    root.title('Captcha')
     root.geometry('400x400')
 
     canvas = Canvas(root, width=300, height=300)
@@ -85,15 +72,63 @@ def captcha():
     text_label.pack(side=BOTTOM)
 
     def on_click(event, shape, color):
-        print(f"You clicked on a {color} {shape}.")
+        global wronged
+        global notRobot
+        # print(f"You clicked on a {color} {shape}.")
         clicked = f"{color} {shape}"
         if clickOn == clicked:
-            print("Human Verified")
+            notRobot = True
+            root.destroy()
+            runNextFunction()
         else:
-            print("FALSE")
-
+            wronged = True
+            root.destroy()
+            
+    
     root.mainloop()
 
 
+while notRobot == False:
+    window = Tk()
+    window.title('Verify You Are Not A Robot')
+    window.geometry('300x300')
+    window.protocol("WM_DELETE_WINDOW", on_window_close)
+    canvas = Canvas(window, width=250, height=250, bg="light gray")
+    canvas.pack()
+    square = canvas.create_rectangle(60, 60, 220, 100, fill="white", width=0)
+    # create the square shape
+    square = canvas.create_rectangle(60, 60, 100, 100, fill="white")
+    if wronged == True:
+        textLabel4 = Label(window, text="Wrong!")
+        textLabel4.config(bg='light gray', fg='red')
+        textLabel4.place(x=125, y=10)
 
-window.mainloop()
+        
+    textLabel = Label(window, text="I Am Not A Robot")
+    textLabel.config(bg='white')
+    textLabel.place(x=130, y=70)
+
+    # define the checkmark line coordinates
+    checkmark_coords = [(70, 80), (80, 90), (95, 70)]
+
+    # create a boolean variable to keep track of whether the checkbox is checked
+    checked = False
+
+    # create a function to handle checkbox clicks
+    def on_checkbox_click(event):
+        global checked
+        if not checked:
+            # draw the checkmark line on top of the square
+            canvas.create_line(*checkmark_coords, width=3, fill="black")
+            checked = True
+            textLabel = Label(window, text="Please Wait...           ")
+            textLabel.config(bg='white')
+            textLabel.place(x=130, y=70)
+            canvas.after(2000, captcha)
+
+
+    # bind the click event to the square
+    canvas.tag_bind(square, "<Button-1>", on_checkbox_click)
+
+    window.mainloop()
+
