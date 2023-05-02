83d48a430bc1d3d3ed2fdf975989919d5a3793ee
judahstarkenburg09 <judahstarkenburg@gmail.com>
2023-05-02 12:21:12 -0600

    add to 100 complete

--- gameApp/addTo100.py
+++ gameApp/addTo100.py
@@ -2,8 +2,49 @@ import tkinter as tk
 
 # Initialize player variable and board list
 player = 1
-
+disableGame = False
 total = 0
+
+root = tk.Tk()
+root.geometry('350x100')
+root.title("ENTER PLAYER NAMES")
+
+def saveNames():
+    global player1entry, player1Name, player2entry, player2Name
+    if player1entry.get() == "":
+        player1Name = "Player 1"
+    else:
+        player1Name = player1entry.get()
+
+    if player2entry.get() == "":
+        player2Name = "Player 2"
+    else:
+        player2Name = player2entry.get()
+    root.destroy()
+
+
+login_frame = tk.Frame(root)
+login_frame.pack(padx=20, pady=20)
+
+player1entry = tk.Entry(login_frame)
+player1entry.pack(side=tk.LEFT, padx=5)
+
+player2entry = tk.Entry(login_frame)
+player2entry.pack(side=tk.LEFT, padx=5)
+
+login_button = tk.Button(login_frame, text="Play", command=saveNames)
+login_button.pack(side=tk.LEFT, padx=5)
+
+def on_closing():
+    exit()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+root.mainloop()
+
+
+
+
 
 def switch_player():
     global player
@@ -13,49 +54,82 @@ def switch_player():
         player = 1
 
 def on_key_press(event):
-    global player, gray_rect_id, total
+    global player, gray_rect_id, total, totalText, goalORwinText, disableGame, turnText
+    
     number = event.char
-    if number.isdigit() and int(number) in range(1, 10):
-        print(f"Player {player}: {number}")
-        total = total + int(number)
-        print(f"Total: {total}")
-        if total >= 100:
+    if disableGame == False:
+        if number.isdigit() and int(number) in range(1, 10):
+            print(f"Player {player}: {number}")
+            total = total + int(number)
+            print(f"Total: {total}")
+            if total >= 50:
+                canvas.delete(goalORwinText)
+                if player == 1:
+                    goalORwinText = canvas.create_text(200, 230, text=f"Winner Is {player2Name}!", font=("Arial", 25))
+                    disableGame = True
+                else:
+                    goalORwinText = canvas.create_text(200, 230, text=f"Winner Is {player1Name}!", font=("Arial", 25))  
+                    disableGame = True
+                gray_rect_id_OVER = canvas.create_polygon(200, 0, 400, 0, 400, 200, 200, 200, fill="#000000", outline="", stipple="gray75")
+                gray_rect_id_OVER = canvas.create_polygon(0, 0, 200, 0, 200, 200, 0, 200, fill="#000000", outline="", stipple="gray75")
+                canvas.delete(turnText)
+
+
+
+            if gray_rect_id is not None:
+                canvas.delete(gray_rect_id)
+                gray_rect_id = None
+            switch_player()
+            # Add gray overlay to non-active player's side
             if player == 1:
-                print("Winner is Player 2!")
+                gray_rect_id = canvas.create_polygon(200, 0, 400, 0, 400, 200, 200, 200, fill="#000000", outline="", stipple="gray75")
+
             else:
-                print("Winner is Player 1!")
-
-
-        if gray_rect_id is not None:
-            canvas.delete(gray_rect_id)
-            gray_rect_id = None
-        switch_player()
-        # Add gray overlay to non-active player's side
-        if player == 1:
-            gray_rect_id = canvas.create_polygon(200, 0, 400, 0, 400, 200, 200, 200, fill="#808080", outline="", stipple="gray50")
-
-        else:
-            gray_rect_id = canvas.create_polygon(0, 0, 200, 0, 200, 200, 0, 200, fill="#808080", outline="", stipple="gray50")
+                gray_rect_id = canvas.create_polygon(0, 0, 200, 0, 200, 200, 0, 200, fill="#000000", outline="", stipple="gray75")
+            canvas.delete(totalText)
+            totalText = canvas.create_text(200, 320, text=f"{total}", font=("Arial", 40))
+            canvas.delete(turnText)
+            if player == 1:
+                whoseTurn = player1Name
+            else:
+                whoseTurn = player2Name
+            turnText = canvas.create_text(200, 370, text=f"{whoseTurn} Turn", font=("Arial", 25))
 
 
 
 
 # Initialize Tkinter window and canvas
 window = tk.Tk()
-window.title("Add To 100")
-window.geometry("400x300")
-canvas = tk.Canvas(window, width=400, height=200)
+window.title("Add To 50")
+window.geometry("400x400")
+canvas = tk.Canvas(window, width=1000, height=1000)
 canvas.pack()
 
+
+
+
+if player == 1:
+    whoseTurn = player1Name
+else:
+    whoseTurn = player2Name
+
+
+
 # Add rectangles to represent each player's half of the canvas
-canvas.create_rectangle(0, 0, 200, 200, fill="red")
-canvas.create_rectangle(200, 0, 400, 200, fill="blue")
+canvas.create_rectangle(0, 0, 200, 200, fill="#FCC300")
+canvas.create_rectangle(200, 0, 400, 200, fill="#1BFF00")
 
 # Add labels to identify each player
-canvas.create_text(100, 50, text="Player 1", font=("Arial", 20))
-canvas.create_text(300, 50, text="Player 2", font=("Arial", 20))
+canvas.create_text(100, 50, text=f"{player1Name}", font=("Arial", 20))
+canvas.create_text(300, 50, text=f"{player2Name}", font=("Arial", 20))
 
-gray_rect_id = canvas.create_polygon(200, 0, 400, 0, 400, 200, 200, 200, fill="#808080", outline="", stipple="gray50")
+totalText = canvas.create_text(200, 320, text=f"{total}", font=("Arial", 40))
+turnText = canvas.create_text(200, 370, text=f"{whoseTurn} Turn", font=("Arial", 25))
+goalORwinText = canvas.create_text(200, 250, text="""         The Goal Of The Game Is:
+Force The Other Player To Add to 50!
+            Type a Number 1-9...""", font=("Arial", 15))
+
+gray_rect_id = canvas.create_polygon(200, 0, 400, 0, 400, 200, 200, 200, fill="#000000", outline="", stipple="gray75")
 
 # Bind key press event to the window
 window.bind("<KeyPress>", on_key_press)
--- projects/test.py
+++ projects/test.py
@@ -1,23 +1,19 @@
 import tkinter as tk
-whitish = '#B6B6B6' #Order from lightest to darkest
-light_gray = '#8A8A8A'
-lighterish_gray = '#4D4D4D'
-lightish_gray = '#3F3F3F'
-darkGray = '#343434'
-hotPinkRed = '#FF69B4'
-font = ("Arial", 15)
+def toggleViewPassword():
+    global hidenPassword, checkbutton
+    if checkbutton.var.get():
+        hidenPassword.config(show = "*")
+    else:
+        hidenPassword.config(show = "")
 
-# Create a Tkinter window
+
 root = tk.Tk()
-root.geometry("500x400")
-root.config(bg=darkGray)
-
-# Create a canvas to draw on
-canvas = tk.Canvas(root, width=2000, height=2000)
-canvas.config(bg=darkGray)
-canvas.pack()
-
-rect1 = canvas.create_rectangle(50, 100, 200, 150, fill=lightish_gray, outline=lighterish_gray, width=.5)
-canvas.create_arc(100, 134, 150, 200, start=90, extent=-180, fill=None, outline=light_gray, style="arc", width=1.5)
-
-root.mainloop()
+hidenPassword = tk.Entry(root)
+hidenPassword.default_show_val = hidenPassword.config(show = "*")
+hidenPassword.config(show = "*")
+checkbutton = tk.Checkbutton(root, text="Show password", onvalue=False, offvalue=True, command=toggleViewPassword)
+checkbutton.var = tk.BooleanVar(value=True)
+checkbutton['variable'] = checkbutton.var
+hidenPassword.pack()
+checkbutton.pack()
+tk.mainloop()
