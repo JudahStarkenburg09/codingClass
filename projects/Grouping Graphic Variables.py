9f5ec06abb1c3128d0ea4cdd5f9be790e18434ba
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-05-17 12:11:18 -0600

    home page done, add description though, and add clickbinds to the iconds

--- projects/Grouping Graphic Variables.py
+++ projects/Grouping Graphic Variables.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 light_gray = '#8A8A8A'
+darkGray = '#343434'
 clicks = 0
 # Define the function to be executed when the button is clicked
 
@@ -11,11 +12,13 @@ root.geometry("700x500")
 canvas = tk.Canvas(root, width=700, height=500)
 canvas.pack()
 
-left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags="home")
-right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags="home")
+rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags="menuIco")
 
-# Create the roof triangle of the home symbol
-roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags="home")
+# Create the horizontal lines
+line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags="menuIco")
+line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags="menuIco")
+line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags="menuIco")
+
 canvas.scale("home", 1, 1, 0.5, 0.4)
 
 # Start the Tkinter main loop
