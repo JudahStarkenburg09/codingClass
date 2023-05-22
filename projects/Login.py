b46cba01ad143ed54168ba26a6819644a90a7835
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-05-22 12:17:29 -0600

    Switching between menus work!!!

--- projects/Login.py
+++ projects/Login.py
@@ -13,8 +13,151 @@ font = ("Arial", 15)
     
 
 
-def switchMenu(current_menu):
-    ''
+def runApp():
+    global username
+    canvas.delete("all")
+
+    rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+    rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+    welcomeText = canvas.create_text(250, 25, text=f"Welcome, {username}", font=font, fill=light_gray)
+    left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+    right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+    roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+    homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill="blue", tags=("home", "bar"))
+    homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+    canvas.scale("home", 1, 1, 0.33, 0.28)
+    canvas.move("home", -3, 90)
+
+    circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+    half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+    line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+    userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+    userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags=("userSymbol", "bar"))
+    canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+    canvas.move("userSymbol", 0, 140)
+
+    rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+    line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+    line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+    line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+    menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+    menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags=("menuIco", "bar"))
+    canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+    canvas.move("menuIco", -5, 200)
+
+    def switch_to_home_screen():
+        print("home")
+        canvas.itemconfigure("homeIcoUnderscoreFalse", fill=light_gray)
+        canvas.itemconfigure("homeIcoUnderscoreTrue", fill="blue")
+        canvas.delete("all")
+        rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+        rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+        welcomeText = canvas.create_text(250, 25, text=f"Welcome, {username}", font=font, fill=light_gray)
+        left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+        right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+        roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+        homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill="blue", tags=("home", "bar"))
+        homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+        canvas.scale("home", 1, 1, 0.33, 0.28)
+        canvas.move("home", -3, 90)
+
+        circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+        userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+        userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags=("userSymbol", "bar"))
+        canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+        canvas.move("userSymbol", 0, 140)
+
+        rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+        line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+        menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags=("menuIco", "bar"))
+        canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+        canvas.move("menuIco", -5, 200)
+        canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+        canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+        canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+        # Additional logic to update the graphics for the home screen
+
+    def switch_to_user_symbol_screen():
+        print("user")
+        canvas.itemconfigure("userIcoUnderscoreFalse", fill=light_gray)
+        canvas.itemconfigure("userIcoUnderscoreTrue", fill="blue")
+        canvas.delete("all")
+        rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+        rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+
+        left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+        right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+        roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+        homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill=light_gray, tags=("home", "bar"))
+        homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+        canvas.scale("home", 1, 1, 0.33, 0.28)
+        canvas.move("home", -3, 90)
+
+        circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+        userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+        userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill="blue",tags=("userSymbol", "bar"))
+        canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+        canvas.move("userSymbol", 0, 140)
+
+        rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+        line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+        menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags=("menuIco", "bar"))
+        canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+        canvas.move("menuIco", -5, 200)
+        canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+        canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+        canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+        # Additional logic to update the graphics for the user symbol screen
+
+    def switch_to_menu_ico_screen():
+        print("menu")
+        canvas.delete("all")
+        rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+        rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+
+        left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+        right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+        roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+        homeIcoUnderscoreFalse = canvas.create_line(50, 163, 120, 163, fill=light_gray, tags=("home", "bar"))
+        homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+        canvas.scale("home", 1, 1, 0.33, 0.28)
+        canvas.move("home", -3, 90)
+
+        circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+        userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+        userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags=("userSymbol", "bar"))
+        canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+        canvas.move("userSymbol", 0, 140)
+
+        rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+        line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+        menuIcoUnderscoreTrue = canvas.create_line(50, 163, 150, 163, fill="blue", tags=("menuIco", "bar"))
+        canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+        canvas.move("menuIco", -5, 200)
+        canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+        canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+        canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+        # Additional logic to update the graphics for the menu icon screen
+
+    # Bind click events to rectangles using lambda functions
+    canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+    canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+    canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
 
         
 
@@ -195,6 +338,7 @@ canvas.bind("<Motion>", on_mouse_move)
 import re
 from tkinter import messagebox
 def click(event):
+    global username, password
     username = username_entry.get()
     password = password_entry.get()
     expression = r'(.*).....$'  # Match any characters before the last 5 characters
@@ -202,8 +346,7 @@ def click(event):
     if password.lower() != str('password'):
         if any(char.isdigit() for char in password):
             if len(password) >= 8:
-                current_menu = "home"
-                switchMenu(current_menu)
+                runApp()
 
             else:
                 messagebox.showwarning("Error", "Your password must be at least 8 characters long!")
--- projects/test.py
+++ projects/test.py
@@ -7,76 +7,159 @@ darkGray = '#343434'
 hotPinkRed = '#FF69B4'
 font = ("Arial", 15)
 
-def switch_screen(screen):
-    current_screen = screen
-    if current_screen == 1:
-        # canvas.delete("all")
-        create_screen1()
-        # userIcoUnderscoreFalse.config(fill="blue")
-        # homeIcoUnderscoreTrue.config(fill="white")
-        # menuIcoUnderscoreFalse.config(fill="white")
-    elif current_screen == 2:
-        # canvas.delete("all")
-        create_screen2()
-        # userIcoUnderscoreFalse.config(fill="white")
-        # homeIcoUnderscoreTrue.config(fill="blue")
-        # menuIcoUnderscoreFalse.config(fill="white")
-    elif current_screen == 3:
-        # canvas.delete("all")
-        create_screen3()
-        # userIcoUnderscoreFalse.config(fill="white")
-        # homeIcoUnderscoreTrue.config(fill="white")
-        # menuIcoUnderscoreFalse.config(fill="blue")
-
-def create_screen1():
-    # Graphics for screen 1
-    print("screen1")
-    
-
-def create_screen2():
-    # Graphics for screen 2
-    print("screen2")
-
-def create_screen3():
-    # Graphics for screen 3
-    print("screen3")
 
 root = tk.Tk()
 root.title("Screen Switching App")
 
 canvas = tk.Canvas(root, width=500, height=500)
+canvas.config(bg=darkGray)
 canvas.pack()
 
-rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="")
-rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="")
+def runApp():
+    rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+    rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
 
-left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags="home")
-right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags="home")
-roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags="home")
-homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill="blue", tags="home")
-homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags="home")
-canvas.scale("home", 1, 1, 0.33, 0.28)
-canvas.move("home", -3, 90)
+    left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+    right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+    roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+    homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill="blue", tags=("home", "bar"))
+    homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+    canvas.scale("home", 1, 1, 0.33, 0.28)
+    canvas.move("home", -3, 90)
 
-circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol'))
-half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol'))
-line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol'))
-userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags="userSymbol")
-userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags="userSymbol")
-canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
-canvas.move("userSymbol", 0, 140)
+    circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+    half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+    line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+    userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+    userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags=("userSymbol", "bar"))
+    canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+    canvas.move("userSymbol", 0, 140)
 
-rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags="menuIco")
-line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags="menuIco")
-line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags="menuIco")
-line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags="menuIco")
-menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags="menuIco")
-menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags="menuIco")
-canvas.scale("menuIco", 1, 1, 0.3, 0.3)
-canvas.move("menuIco", -5, 200)
+    rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+    line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+    line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+    line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+    menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+    menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags=("menuIco", "bar"))
+    canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+    canvas.move("menuIco", -5, 200)
 
-canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_screen(1))
-canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_screen(2))
-canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_screen(3))
+    def switch_to_home_screen():
+        print("home")
+        canvas.itemconfigure("homeIcoUnderscoreFalse", fill=light_gray)
+        canvas.itemconfigure("homeIcoUnderscoreTrue", fill="blue")
+        canvas.delete("all")
+        rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+        rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+
+        left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+        right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+        roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+        homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill="blue", tags=("home", "bar"))
+        homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+        canvas.scale("home", 1, 1, 0.33, 0.28)
+        canvas.move("home", -3, 90)
+
+        circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+        userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+        userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags=("userSymbol", "bar"))
+        canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+        canvas.move("userSymbol", 0, 140)
+
+        rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+        line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+        menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags=("menuIco", "bar"))
+        canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+        canvas.move("menuIco", -5, 200)
+        canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+        canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+        canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+        # Additional logic to update the graphics for the home screen
+
+    def switch_to_user_symbol_screen():
+        print("user")
+        canvas.itemconfigure("userIcoUnderscoreFalse", fill=light_gray)
+        canvas.itemconfigure("userIcoUnderscoreTrue", fill="blue")
+        canvas.delete("all")
+        rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+        rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+
+        left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+        right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+        roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+        homeIcoUnderscoreTrue = canvas.create_line(50, 163, 120, 163, fill=light_gray, tags=("home", "bar"))
+        homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+        canvas.scale("home", 1, 1, 0.33, 0.28)
+        canvas.move("home", -3, 90)
+
+        circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+        userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+        userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill="blue",tags=("userSymbol", "bar"))
+        canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+        canvas.move("userSymbol", 0, 140)
+
+        rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+        line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+        menuIcoUnderscoreFalse = canvas.create_line(50, 163, 150, 163, fill=light_gray, tags=("menuIco", "bar"))
+        canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+        canvas.move("menuIco", -5, 200)
+        canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+        canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+        canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+        # Additional logic to update the graphics for the user symbol screen
+
+    def switch_to_menu_ico_screen():
+        print("menu")
+        canvas.delete("all")
+        rect1bar = canvas.create_rectangle(0, 0, 50, 500, fill=lightish_gray, outline="", tags='bar')
+        rect2bar = canvas.create_rectangle(0, 0, 500, 50, fill=lightish_gray, outline="", tags='bar')
+
+        left_rectangle = canvas.create_rectangle(50, 100, 75, 150, fill=light_gray, outline="",tags=("home", "bar"))
+        right_rectangle = canvas.create_rectangle(95, 100, 120, 150, fill=light_gray, outline="", tags=("home", "bar"))
+        roof_triangle = canvas.create_polygon(30, 100, 85, 30, 140, 100, fill=light_gray, outline="", tags=("home", "bar"))
+        homeIcoUnderscoreFalse = canvas.create_line(50, 163, 120, 163, fill=light_gray, tags=("home", "bar"))
+        homeRectBind = canvas.create_rectangle(30, 30, 140, 155, fill=None, width=0, tags=("home", "bar"))
+        canvas.scale("home", 1, 1, 0.33, 0.28)
+        canvas.move("home", -3, 90)
+
+        circle = canvas.create_oval(66.7, 66.7, 133.3, 133.3, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        half_circle = canvas.create_arc(50, 134, 150, 234, start=0, extent=180, fill=light_gray, outline=light_gray, width=1.5, tags=('userSymbol', "bar"))
+        line1 = canvas.create_line(48, 184, 152, 184, width=1.5, fill=light_gray, tags=('userSymbol', "bar"))
+        userSymbolRectBind = canvas.create_rectangle(45, 62.7, 155, 187, fill=None, width=0 ,tags=("userSymbol", "bar"))
+        userIcoUnderscoreFalse = canvas.create_line(48, 197, 152, 197, fill=light_gray,tags=("userSymbol", "bar"))
+        canvas.scale("userSymbol", 1, 1, 0.25, 0.25)
+        canvas.move("userSymbol", 0, 140)
+
+        rectIco = canvas.create_rectangle(50, 50, 150, 150, fill=light_gray, outline="", tags=("menuIco", "bar"))
+        line1Ico = canvas.create_line(60, 80, 140, 80, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line2Ico = canvas.create_line(60, 100, 140, 100, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        line3Ico = canvas.create_line(60, 120, 140, 120, width=4, fill=darkGray, tags=("menuIco", "bar"))
+        menuIcoSymbolRectBind = canvas.create_rectangle(50, 50, 150, 150, fill=None, outline="", width=0 ,tags=("menuIco", "bar"))
+        menuIcoUnderscoreTrue = canvas.create_line(50, 163, 150, 163, fill="blue", tags=("menuIco", "bar"))
+        canvas.scale("menuIco", 1, 1, 0.3, 0.3)
+        canvas.move("menuIco", -5, 200)
+        canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+        canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+        canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+        # Additional logic to update the graphics for the menu icon screen
+
+    # Bind click events to rectangles using lambda functions
+    canvas.tag_bind(homeRectBind, "<Button-1>", lambda event: switch_to_home_screen())
+    canvas.tag_bind(userSymbolRectBind, "<Button-1>", lambda event: switch_to_user_symbol_screen())
+    canvas.tag_bind(menuIcoSymbolRectBind, "<Button-1>", lambda event: switch_to_menu_ico_screen())
+
+
+input('')
+runApp()
 
 root.mainloop()
