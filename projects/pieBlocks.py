55907ad09c954d97154f214d890defb6d98a2f7b
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-08-09 14:23:19 -0600

    pie blocks dont work!

--- projects/pieBlocks.py
+++ projects/pieBlocks.py
@@ -1,137 +1,141 @@
 import pygame
 import sys
-import tkinter as tk
+import math
+from pygame.locals import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
 
-def save_answer():
-    global answer
-    answer = entry.get()
-    weight.destroy()
-
-def on_enter(event):
-    save_answer()
-
-def on_closing():
-    # Any cleanup or saving actions can be placed here
-    exit()
-
-weight = tk.Tk()
-weight.geometry('300x200')
-weight.title('Weight')
-weight.protocol("WM_DELETE_WINDOW", on_closing)
-
-question = tk.Label(weight, text="Enter the amount of weight for the block (kg)")
-question.place(x=5, y=20)
-
-entry = tk.Entry(weight)
-entry.pack(padx=20, pady=50)
-entry.bind("<Return>", on_enter)
-
-
-weight.mainloop()
-
-
-
-
-
-
-# Initialize pygame
+# Set up the screen
 pygame.init()
-
-# Set up display
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Collision Simulation")
+clock = pygame.time.Clock()
 
-# Colors
-white = (255, 255, 255)
-blue = (0, 0, 255)
-red = (255, 0, 0)
+glMatrixMode(GL_PROJECTION)
+glLoadIdentity()
+gluOrtho2D(0, screen_width, 0, screen_height)
+# glOrtho(-100, screen_width, -100, screen_height, -1, 1)
+glMatrixMode(GL_MODELVIEW)
 
-# Create blocks
-block1_mass = 1
-block2_mass_red = int(answer)
-speed = 1
+# Create block class
+class Block:
+    def __init__(self, mass, pos, vel, color):
+        self.mass = mass
+        self.pos = pos
+        self.vel = vel
+        self.color = color
+        if len(str(self.mass)) == 1:
+            self.size = len(str(self.mass)) * 40
+        else:
+            self.size = len(str(self.mass)) * 10
 
+    def render(self):
+        pygame.draw.rect(screen, self.color, (self.pos, (screen_height/2) - self.size, self.size, self.size))
 
-block1_width = 40
-block1_height = 40
-block1_x = 50
-block1_y = screen_height - block1_height
-block1_vx = 0
+    def update(self, dt):
+        self.pos += self.vel * dt
 
-block2_width = 40
-block2_height = 40
-block2_x = 300
-block2_y = screen_height - block2_height
-block2_vx = -speed
-collision_count = 0
+# Instantiate two blocks
+b1Pos = 200  
+b1Mass = 1
 
-# Create wall
-wall_color = (0, 0, 0)
-wall_thickness = 5
-black = (0,0,0)
-# Create font
-font1 = pygame.font.Font(None, 20)
-font2 = pygame.font.Font(None, 30)
+user_input = ''
+user_prompt = True
 
+while user_prompt:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == KEYDOWN:
+            if event.key == K_RETURN:
+                user_prompt = False
+        elif event.type == KEYUP:
+            if event.key == K_BACKSPACE:
+                user_input = user_input[:-1]
+            else:
+                user_input += event.unicode
 
-def draw_wall():
-    pygame.draw.line(screen, wall_color, (0, 0), (0, screen_height), wall_thickness)
+    screen.fill((0, 0, 0))  # Set the background color to black
+    font = pygame.font.Font(None, 30)
+    text = font.render(f"Enter number of digits in pi (after 3.) you want to check: {user_input}", True, (255, 255, 255))
+    screen.blit(text, (10, 10))
+    pygame.display.update()
 
-def draw_weights():
-    text_block1 = font1.render(f"{block1_mass} kg", True, black)
-    text_block2 = font1.render(f"{block2_mass_red} kg", True, black )
-    screen.blit(text_block1, (block1_x +5, block1_y-20))
-    screen.blit(text_block2, (block2_x +5, block2_y-20))
+b2Mass = 100 ** int(user_input)
+print(f"Second mass is {b2Mass}")
+b2Pos = 300  
+b2vel = -500
 
-def update_velocity(m1, v1, m2, v2):
-    new_v1 = ((m1 - m2) / (m1 + m2)) * v1 + (2 * m2 / (m1 + m2)) * v2
-    new_v2 = (2 * m1 / (m1 + m2)) * v1 + ((m2 - m1) / (m1 + m2)) * v2
-    return new_v1, new_v2
+# Calculate the value of pi based on the user's input
+digits = int(user_input)
+pi_value = str(math.pi)[:2+digits]  # Get the specified number of digits from pi
 
-# Main loop
-def main():
-    global block1_x, block2_x, block1_vx, block2_vx, block1_mass, block2_mass_red, collision_count
-    clock = pygame.time.Clock()
+block1 = Block(b1Mass, b1Pos, 0, (0, 0, 255))
+block2 = Block(b2Mass, b2Pos, b2vel, (255, 0, 0))
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+# Main game loop
+count = 0
+dt = 0.001
+running = True
 
-        screen.fill(white)
-        draw_wall()
+# Add a horizontal line
+line_y = screen_height // 2  # Adjust the y-coordinate of the line
+line_color = (255, 255, 255)  # Set the color of the line to white
 
-        # Update block positions
-        block1_x += block1_vx
-        block2_x += block2_vx
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        # Check collisions
-    
-        if block1_x + block1_width >= block2_x:
-            block1_vx, block2_vx = update_velocity(block1_mass, block1_vx, block2_mass_red, block2_vx)
-            collision_count += 1
+    # Update block positions
+    block1.update(dt)
+    block2.update(dt)
 
-        if block1_x <= 0:
-            block1_vx *= -1
-            collision_count +=1
+    # Check wall collision
+    if block1.pos <= 0:
+        block1.pos = 0
+        block1.vel *= -1
+        count += 1
 
-        if block2_x <= 0:
-            block2_vx *= -1
+    # Check block collision
+    if block1.pos + block1.size >= block2.pos:
+        block1.pos = block2.pos - block1.size
 
-        # Draw blocks
-        pygame.draw.rect(screen, blue, (block1_x, block1_y, block1_width, block1_height))
-        pygame.draw.rect(screen, red, (block2_x, block2_y, block2_width, block2_height))
+        m1 = block1.mass
+        m2 = block2.mass
+        v1i = block1.vel
+        v2i = block2.vel
 
-        text_collision_count = font2.render(f"Collisions: {collision_count}", True, (0, 0, 0))
-        screen.blit(text_collision_count, (20, 100))
-        draw_weights()
+        mom1 = m1 * v1i + m2 * v2i
 
-        pygame.display.update()
-        clock.tick(60)
+        v2f = (m1 * v2i - m1 * v1i - mom1) / (-1 * m2 - m1)
+        v1f = v2i + v2f - v1i
 
+        block1.vel = v1f
+        block2.vel = v2f
+        count += 1
 
+    # Render the frame
+    screen.fill((0, 0, 0))  # Set the background color to black
+    font = pygame.font.Font(None, 30)
+    text = font.render(f"Collisions: {count}", True, (255, 255, 255))  # Set text color to white
+    screen.blit(text, (10, 10))
 
-main()
+    # Render the pi value text
+    text_pi = font.render(f"Expected pi value: {pi_value}", True, (255, 255, 255))
+    screen.blit(text_pi, (10, 50))
+
+    # Display mass value
+    text = font.render(f"M1: {b1Mass}   M2: {b2Mass}", True, (255, 255, 255))
+    screen.blit(text, (10, (screen_height/2) + 30))  # Adjusted text position
+
+    # Draw the horizontal line
+    pygame.draw.line(screen, line_color, (0, line_y), (screen_width, line_y), 1)
+
+    block1.render()
+    block2.render()
+
+    pygame.display.flip()
+    clock.tick(60)
