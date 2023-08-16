9bc1bf6c51c420194bffecc5a4a98d9f20344404
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-08-16 11:57:37 -0600

    select buttons for buttons 123

--- projects/test2.py
+++ projects/test2.py
@@ -1,9 +1,91 @@
-from tkinter import *
-from PIL import ImageTk,Image
-root = Tk()
-root.geometry('800x800')
-canvas = Canvas(root, width = 1000, height = 1000)
+import tkinter as tk
+import random
+from tkinter import messagebox
+
+colors = ['red', 'orange', 'yellow', 'blue']
+color_counts = {color: 0 for color in colors}
+color_limit = 6
+
+result = []
+
+while True:
+    if all(count == color_limit for count in color_counts.values()):
+        break
+    
+    rColor = random.choice(colors)
+    if color_counts[rColor] < color_limit:
+        result.append(rColor)
+        color_counts[rColor] += 1
+
+def pour_color(vial_index):
+    selected_vial = vial_buttons[vial_index]
+    if selected_vial['color_count'] < color_limit:
+        selected_color = selected_vial['color']
+        if selected_color:
+            result_color = result.pop(0)
+            selected_vial['color_count'] += 1
+            selected_vial['color'] = result_color
+            update_vial(selected_vial)
+            check_win_condition()
+
+def clear_selection():
+    for vial in vial_buttons:
+        vial['selected'] = False
+        update_vial(vial)
+
+def update_vial(vial):
+    canvas.itemconfig(vial['color_rectangle'], fill=vial['color'])
+    if vial['color_count'] >= color_limit:
+        canvas.itemconfig(vial['button'], state=tk.DISABLED)
+    else:
+        canvas.itemconfig(vial['button'], state=tk.NORMAL)
+    if vial['selected']:
+        canvas.itemconfig(vial['button'], bg='green')
+    else:
+        canvas.itemconfig(vial['button'], bg='SystemButtonFace')
+
+def check_win_condition():
+    win = all(vial['color'] == vial['color'] * color_limit for vial in vial_buttons)
+    if win:
+        messagebox.showinfo("Congratulations!", "You've won the game!")
+
+def vial_button_click(vial_index):
+    clear_selection()
+    vial_buttons[vial_index]['selected'] = True
+    update_vial(vial_buttons[vial_index])
+
+root = tk.Tk()
+root.geometry('500x400')
+root.title('Water Sort Puzzle')
+root.config(bg='light gray')
+
+canvas = tk.Canvas(root, width=500, height=400, bg='white')
 canvas.pack()
-img = ImageTk.PhotoImage(Image.open("donut.png"))
-canvas.create_image(20, 20, anchor=NW, image=img)
+
+vial_buttons = []
+
+for i in range(4):
+    vial_color = result.pop(0)
+    vial = {
+        'color': vial_color,
+        'color_count': 1,
+        'selected': False,
+    }
+    
+    vial['color_rectangle'] = canvas.create_rectangle(
+        50 + i * 115, 150, 150 + i * 115, 350,
+        fill='white', outline='black', width=1.5
+    )
+    
+    vial['button'] = tk.Button(
+        root, text='Vial {}'.format(i + 1),
+        command=lambda index=i: vial_button_click(index)
+    )
+    vial['button'].place(x=50 + i * 115, y=110, width=100, height=30)
+    
+    vial_buttons.append(vial)
+
+clear_button = tk.Button(root, text='Clear', command=clear_selection)
+clear_button.place(x=10, y=350, width=80, height=30)
+
 root.mainloop()
--- projects/waterSortPuzzle.py
+++ projects/waterSortPuzzle.py
@@ -13,6 +13,66 @@ color_counts = {color: 0 for color in colors}
 color_limit = 4
 
 result = []
+selected = None
+def clear_selection():
+    global selected, selectV1_button, selectV2_button, selectV3_button, selected
+
+    selectV1_button.destroy()
+    selectV1_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=1))
+    selectV1_button.place(x=40, y=20, width=80, height=30)
+    selectV2_button.destroy()
+    selectV2_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=2))
+    selectV2_button.place(x=140, y=20, width=80, height=30)
+    selectV3_button.destroy()
+    selectV3_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=3))
+    selectV3_button.place(x=240, y=20, width=80, height=30)
+
+
+    selected = None
+    print(selected)
+
+
+
+def select(selected):
+    global select, selectV1_button, selectV2_button, selectV3_button
+
+    pour()
+
+    selectV1_button.destroy()
+    selectV1_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=1))
+    selectV1_button.place(x=40, y=20, width=80, height=30)
+    selectV2_button.destroy()
+    selectV2_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=2))
+    selectV2_button.place(x=140, y=20, width=80, height=30)
+    selectV3_button.destroy()
+    selectV3_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=3))
+    selectV3_button.place(x=240, y=20, width=80, height=30)
+    
+
+    if selected == 1:
+        selectV1_button.destroy()
+        selectV1_button = tk.Button(root, text='Select', bg='green', command=lambda: select(selected=1))
+        selectV1_button.place(x=40, y=20, width=80, height=30)
+    elif selected == 2:
+        selectV2_button.destroy()
+        selectV2_button = tk.Button(root, text='Select', bg='green', command=lambda: select(selected=2))
+        selectV2_button.place(x=140, y=20, width=80, height=30)
+    elif selected == 3:
+        selectV3_button.destroy()
+        selectV3_button = tk.Button(root, text='Select', bg='green', command=lambda: select(selected=3))
+        selectV3_button.place(x=240, y=20, width=80, height=30)
+    elif selected == 4:
+        ''
+
+
+    print(selected)
+
+def pour():
+    global select, selectV1_button, selectV2_button, selectV3_button, selected
+    if selected:
+        ''
+    selected = None
+
 
 while True:
     if all(count == color_limit for count in color_counts.values()):
@@ -23,27 +83,106 @@ while True:
         result.append(rColor)
         color_counts[rColor] += 1
 
+white = '#FFFFFF'
+
 rColor1, rColor2, rColor3, rColor4, rColor5, rColor6, rColor7, rColor8, rColor9, rColor10, rColor11, rColor12, rColor13, rColor14, rColor15, rColor16 = result
 print(result)
 
 root = tk.Tk()
-root.geometry('500x300')
+root.geometry('500x400')
 root.title('Water Sort Puzzle')
 root.config(bg='light gray')   
 
-canvas = tk.Canvas(root, width = 1000, height = 1000, bg='light gray')
+canvas = tk.Canvas(root, width = 1000, height = 1000, bg=white)
 canvas.pack()
 
+# region beaker1
 
-canvas.create_arc(50, 200, 105, 134, start=180, extent=180, fill=rColor1, outline='black', width=1.5, tags=('beaker1'))
-canvas.create_rectangle(50, 127, 105, 167, fill=rColor2, width=1.5, outline='black', tags=('beaker'))
-canvas.create_line(51, 167, 104, 167, fill=rColor1, width=1.5, tags=('beaker1'))
-canvas.create_rectangle(50, 87, 105, 127, fill=rColor3, width=1.5, outline='black', tags=('beaker'))
-canvas.create_line(51, 127, 104, 127, fill=rColor2, width=1.5, tags=('beaker1'))
-canvas.create_line(51, 87, 104, 87, fill=rColor3, width=1.5, tags=('beaker1'))
+selectV1_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=1))
+selectV1_button.place(x=40, y=20, width=80, height=30)
+
+canvas.create_arc(50, 300, 105, 234, start=180, extent=180, fill=rColor1, outline='black', width=1.5, tags=('beaker1')) #bottom
+canvas.create_rectangle(50, 227, 105, 267, fill=rColor2, width=1.5, outline='black', tags=('beaker1')) #2nd layer
+canvas.create_line(51, 267, 104, 267, fill=rColor1, width=1.5, tags=('beaker1')) #bottom line
+canvas.create_rectangle(50, 187, 105, 227, fill=rColor3, width=1.5, outline='black', tags=('beaker1')) #3rd layer
+canvas.create_line(51, 227, 104, 227, fill=rColor2, width=1.5, tags=('beaker1')) #2nd line
+canvas.create_rectangle(50, 147, 105, 187, fill=rColor4, width=1.5, outline='black', tags=('beaker1')) #4th layer
+canvas.create_line(51, 187, 104, 187, fill=rColor3, width=1.5, tags=('beaker1')) #3rd line
+canvas.create_rectangle(50, 107, 105, 147, fill=white, width=1.5, outline='black', tags=('beaker1')) #5th layer
+canvas.create_line(51, 147, 104, 147, fill=rColor4, width=1.5, tags=('beaker1')) #4th line
+canvas.create_rectangle(50, 67, 105, 107, fill=white, width=1.5, outline='black', tags=('beaker1')) #6th layer
+canvas.create_line(51, 107, 104, 107, fill=white, width=1.5, tags=('beaker1')) #5th line
+canvas.create_line(51, 67, 104, 67, fill=white, width=1.5, tags=('beaker1')) #6th line
+
+canvas.move('beaker1', 0, 0)
+# endregion
+
+# region beaker2
+
+selectV2_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=2))
+selectV2_button.place(x=140, y=20, width=80, height=30)
+
+canvas.create_arc(50, 300, 105, 234, start=180, extent=180, fill=rColor5, outline='black', width=1.5, tags=('beaker2')) #bottom
+canvas.create_rectangle(50, 227, 105, 267, fill=rColor6, width=1.5, outline='black', tags=('beaker2')) #2nd layer
+canvas.create_line(51, 267, 104, 267, fill=rColor5, width=1.5, tags=('beaker2')) #bottom line
+canvas.create_rectangle(50, 187, 105, 227, fill=rColor7, width=1.5, outline='black', tags=('beaker2')) #3rd layer
+canvas.create_line(51, 227, 104, 227, fill=rColor6, width=1.5, tags=('beaker2')) #2nd line
+canvas.create_rectangle(50, 147, 105, 187, fill=rColor8, width=1.5, outline='black', tags=('beaker2')) #4th layer
+canvas.create_line(51, 187, 104, 187, fill=rColor7, width=1.5, tags=('beaker2')) #3rd line
+canvas.create_rectangle(50, 107, 105, 147, fill=white, width=1.5, outline='black', tags=('beaker2')) #5th layer
+canvas.create_line(51, 147, 104, 147, fill=rColor8, width=1.5, tags=('beaker2')) #4th line
+canvas.create_rectangle(50, 67, 105, 107, fill=white, width=1.5, outline='black', tags=('beaker2')) #6th layer
+canvas.create_line(51, 107, 104, 107, fill=white, width=1.5, tags=('beaker2')) #5th line
+canvas.create_line(51, 67, 104, 67, fill=white, width=1.5, tags=('beaker2')) #6th line
 
 
 
+canvas.move('beaker2', 100, 0)
+# endregion
 
+# region beaker3
+
+selectV3_button = tk.Button(root, text='Select', bg='gray', command=lambda: select(selected=3))
+selectV3_button.place(x=240, y=20, width=80, height=30)
+
+canvas.create_arc(50, 300, 105, 234, start=180, extent=180, fill=rColor9, outline='black', width=1.5, tags=('beaker3')) #bottom
+canvas.create_rectangle(50, 227, 105, 267, fill=rColor10, width=1.5, outline='black', tags=('beaker3')) #2nd layer
+canvas.create_line(51, 267, 104, 267, fill=rColor9, width=1.5, tags=('beaker3')) #bottom line
+canvas.create_rectangle(50, 187, 105, 227, fill=rColor11, width=1.5, outline='black', tags=('beaker3')) #3rd layer
+canvas.create_line(51, 227, 104, 227, fill=rColor10, width=1.5, tags=('beaker3')) #2nd line
+canvas.create_rectangle(50, 147, 105, 187, fill=rColor12, width=1.5, outline='black', tags=('beaker3')) #4th layer
+canvas.create_line(51, 187, 104, 187, fill=rColor11, width=1.5, tags=('beaker3')) #3rd line
+canvas.create_rectangle(50, 107, 105, 147, fill=white, width=1.5, outline='black', tags=('beaker3')) #5th layer
+canvas.create_line(51, 147, 104, 147, fill=rColor12, width=1.5, tags=('beaker3')) #4th line
+canvas.create_rectangle(50, 67, 105, 107, fill=white, width=1.5, outline='black', tags=('beaker3')) #6th layer
+canvas.create_line(51, 107, 104, 107, fill=white, width=1.5, tags=('beaker3')) #5th line
+canvas.create_line(51, 67, 104, 67, fill=white, width=1.5, tags=('beaker3')) #6th line
+
+canvas.move('beaker3', 200, 0)
+
+# endregion
+
+# region beaker4
+
+canvas.create_arc(50, 300, 105, 234, start=180, extent=180, fill=rColor13, outline='black', width=1.5, tags=('beaker4')) #bottom
+canvas.create_rectangle(50, 227, 105, 267, fill=rColor14, width=1.5, outline='black', tags=('beaker4')) #2nd layer
+canvas.create_line(51, 267, 104, 267, fill=rColor13, width=1.5, tags=('beaker4')) #bottom line
+canvas.create_rectangle(50, 187, 105, 227, fill=rColor15, width=1.5, outline='black', tags=('beaker4')) #3rd layer
+canvas.create_line(51, 227, 104, 227, fill=rColor14, width=1.5, tags=('beaker4')) #2nd line
+canvas.create_rectangle(50, 147, 105, 187, fill=rColor16, width=1.5, outline='black', tags=('beaker4')) #4th layer
+canvas.create_line(51, 187, 104, 187, fill=rColor15, width=1.5, tags=('beaker4')) #3rd line
+canvas.create_rectangle(50, 107, 105, 147, fill=white, width=1.5, outline='black', tags=('beaker4')) #5th layer
+canvas.create_line(51, 147, 104, 147, fill=rColor16, width=1.5, tags=('beaker4')) #4th line
+canvas.create_rectangle(50, 67, 105, 107, fill=white, width=1.5, outline='black', tags=('beaker4')) #6th layer
+canvas.create_line(51, 107, 104, 107, fill=white, width=1.5, tags=('beaker4')) #5th line
+canvas.create_line(51, 67, 104, 67, fill=white, width=1.5, tags=('beaker4')) #6th line
+
+canvas.move('beaker4', 300, 0)
+
+# endregion
+
+
+clear_button = tk.Button(root, text='Clear', command=clear_selection)
+clear_button.place(x=10, y=350, width=80, height=30)
 
 root.mainloop()
