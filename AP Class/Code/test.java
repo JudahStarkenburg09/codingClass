9dc3ebbc63d22b9cc7fd708c749acf23fba77e1b
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-08-29 16:02:19 -0600

    2 things learned in Java

--- AP Class/Code/test.java
+++ AP Class/Code/test.java
@@ -1,8 +1,8 @@
-
-public class HelloPrinter
+public class Test
 {
    public static void main(String[] args)
-   {  
-      System.out.println("Hello, World!");
+   {
+      System.out.print("Hello");
+      System.out.println( "World"); // println means print line, therefore printing on same line as before
    }
 }
