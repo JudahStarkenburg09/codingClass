03568c431b3cf119405ef824bf80adbcbbaecd07
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-09-05 06:35:07 -0600

    nothing much

---
+++ AP Class/Code/HN_w1lab.java
@@ -0,0 +1,11 @@
+public class HN_w1lab {
+    public static void main(String[] args)
+   {  
+      System.out.println("**     **        **       ********     **     **         **       **     **");
+      System.out.println("****   **       ****      ********     **     **        ****      ****   **");
+      System.out.println("** **  **      **  **        **        *********       **  **     ** **  **");
+      System.out.println("**  ** **     ********       **        *********      ********    **  ** **");
+      System.out.println("**   ****    **      **      **        **     **     **      **   **   ****");
+      System.out.println("**     **   **        **     **        **     **    **        **  **     **");
+   }
+}
