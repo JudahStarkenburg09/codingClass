d40a0b668f50c6762ccf377bdf90b4444036323b
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-09-06 11:53:24 -0600

    dpne

--- projects/test3.py
+++ projects/test3.py
@@ -1,29 +1,19 @@
-# pip install resquests
-import requests
+import random
 
-def correct_grammar(textToChange):
-    url = "https://languagetool.org/api/v2/check"
-    data = {
-        "text": textToChange,
-        "language": "en-US",  # Change this if needed
-    }
-    
-    response = requests.post(url, data=data)
-    results = response.json()
-    
-    corrected_text = textToChange
-    
-    for match in reversed(results['matches']):
-        replacements = match.get('replacements', [])
-        if replacements:
-            corrected_text = (
-                corrected_text[:match['offset']] +
-                replacements[0]['value'] +
-                corrected_text[match['offset'] + match['length']:]
-            )
-    
-    return corrected_text
+while True:
+    print(random.uniform(0,1.5))
+    input()
 
-input_text = "You is wearing a blue shirt"
-corrected_text = correct_grammar(input_text)
-print(corrected_text)
+
+
+
+
+
+
+
+
+
+
+
+
+    
