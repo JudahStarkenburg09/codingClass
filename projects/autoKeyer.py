bba9a86caf1ff1946d12443fd1b201cde9f68738
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-09-22 11:58:23 -0600

    d

--- projects/autoKeyer.py
+++ projects/autoKeyer.py
@@ -3,10 +3,14 @@ import time
 import tkinter as tk
 
 window = tk.Tk()
-window.geometry('200x200')
+window.geometry('300x200')
 window.title("Auto Keyer")
 
+miliText = tk.Label(window, text="Enter Time (Miliseconds)")
+miliText.place(x=10, y=10)
 
+miliTime = tk.Entry(window, width=7)
+miliTime.place(x=150, y=10)
 
 
 
