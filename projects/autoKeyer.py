f160822820b364b0fc1051ebff38cb4a025db343
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-09-21 20:02:47 -0600

    failed

---
+++ projects/autoKeyer.py
@@ -0,0 +1,13 @@
+import pyautogui
+import time
+import tkinter as tk
+
+window = tk.Tk()
+window.geometry('200x200')
+window.title("Auto Keyer")
+
+
+
+
+
+window.mainloop()
--- projects/autoOpen.py
+++ projects/autoOpen.py
@@ -1,12 +1,12 @@
-import pyautogui
 import time
 
 def main():
     # Sleep for a few seconds to allow the computer to detect the USB drive
-    time.sleep(5)
+    time.sleep(3)
 
     # Simulate typing "hi"
-    pyautogui.typewrite("hi")
+    print("Hello World")
 
+    input("Press Enter To Exit")
 if __name__ == "__main__":
     main()
