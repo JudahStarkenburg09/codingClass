4416142dfcae3fbbbbaa67b8d4e5c49f2230bc55
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-10-11 11:47:32 -0600

    type game almost done

--- projects/typing game/type.py
+++ projects/typing game/type.py
@@ -28,7 +28,7 @@ def ifs(key, nextKey, i):
             tries = 0
         if tries == 1:
             add = {
-                f"{nextKey}": 'yellow'
+                f"{nextKey}": 'orange'
             }
             colors.append(add)
             tries = 0
@@ -38,6 +38,8 @@ def ifs(key, nextKey, i):
             }
             colors.append(add)
             tries = 0
+    else:
+        tries += 1
 
 # Function to handle key presses
 def handleKeyPress(key, nextKey):
@@ -45,17 +47,21 @@ def handleKeyPress(key, nextKey):
 
     if key == pygame.K_SPACE:
         i = ' '
-        ifs(key, nextKey, i)
     elif key in (pygame.K_a, pygame.K_b, pygame.K_c, pygame.K_d, pygame.K_e, pygame.K_f, pygame.K_g,
                  pygame.K_h, pygame.K_i, pygame.K_j, pygame.K_k, pygame.K_l, pygame.K_m, pygame.K_n,
                  pygame.K_o, pygame.K_p, pygame.K_q, pygame.K_r, pygame.K_s, pygame.K_t, pygame.K_u,
                  pygame.K_v, pygame.K_w, pygame.K_x, pygame.K_y, pygame.K_z):
         i = chr(key)
-        ifs(key, nextKey, i)
     else:
         tries += 1
+        return
+    ifs(key, nextKey, i)
+    
 
     print(alreadyTyped)
+
+# Rest of your code remains the same.
+
 
 sentence = typeData.sentenceEquals(25)
 alreadyTyped = ''
@@ -79,15 +85,26 @@ while running:
     font = pygame.font.Font(None, 36)
 
     # Display the sentence in black text
-    sentenceText = font.render(sentence, True, (0, 0, 0))
-    window.blit(sentenceText, (20, 20))
+    xS = 20
+    y= 20
+    for num, i in enumerate(sentence):
+        if xS >= 780:
+            y += 30
+            xS = 20
+        sentenceText = font.render(i, True, (0,0,0))
+        window.blit(sentenceText, (xS, y))
+        xS += sentenceText.get_width()
 
     # Display typed characters with corresponding colors
+    y = 20
     x = 20  # Starting x-coordinate for colored text
     for char, color in zip(alreadyTyped, colors):
+        if x >= 780:
+            y += 30
+            x = 20
         charText = font.render(char, True, pygame.Color(color[char]))
-        window.blit(charText, (x, 60))
-        x += charText.get_width() + 5  # Adjust the spacing
+        window.blit(charText, (x, 20))
+        x += charText.get_width()  # Adjust the spacing
 
     # Update the display
     pygame.display.flip()
