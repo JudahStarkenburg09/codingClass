b5011655f711598c89a1eed78913fbab0ae22370
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-10-26 16:50:00 -0600

    msde

--- Web/Scoreboard/main.py
+++ Web/Scoreboard/main.py
@@ -14,13 +14,16 @@ def onOpen():
 @app.route('/home')
 def index():
     global dropArray, html_content
+    html_content = []
     boostrap = {
         "name": "bootstrap",
         "code": """<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">""",
     }
 
-    html_content = [boostrap]
-    html_content.append(PWD.makeButton(buttonLabel="Sign In", buttonName="button" ,buttonXPos=50, buttonYPos=50, onclick='/sign-in', classType="btn btn-primary"))
+    html_content.append(boostrap)
+    html_content.append(PWD.makeButton(buttonLabel="Start", buttonName="start" ,buttonXPos=50, buttonYPos=100, onclick='/load', classType="btn btn-primary"))
+    html_content.append(PWD.createCheckbox(label="Timer", id="timerCheckbox", checked=False, name="timerCheckbox", posx=50, posy=50))
+    html_content.append(PWD.createCheckbox(label="Posession", id="posessionCheckbox", checked=False, name="posessionCheckbox", posx=50, posy=70))
 
 
     index = """"""
@@ -28,24 +31,10 @@ def index():
         index += str(i["code"])
     return index
 
-# if request.method == 'POST':
-#     print("Got In")
-#     entry_content = request.form.get('entryBox1')
-#     print(f"Entry Box Content: {entry_content}")
-@app.route('/sign-in', methods=['GET', 'POST'])
-def changePage():
-    global html_content
-    for i in html_content:
-        if i["name"] == "button":
-            html_content.remove(i)
+app.route("/load")
+def ifs():
+    ''
 
-
-
-    
-    index = ''
-    for i in html_content:
-        index += str(i["code"])
-    return index
 
 dropArray = [
     {
--- Web/Scoreboard/pyWebDev.py
+++ Web/Scoreboard/pyWebDev.py
@@ -1,11 +1,26 @@
 # https://getbootstrap.com/docs/5.3/getting-started/introduction/
 
-def makeButton(buttonLabel, buttonName, buttonXPos, buttonYPos, onclick, classType=None):
+def makeButton(buttonLabel, buttonName, buttonXPos=None, buttonYPos=None, onclick=None, classType=None):
     html_code = f"""
     <button class="{classType}" style="position: absolute; left: {buttonXPos}px; top: {buttonYPos}px;" onclick="window.location.href='{onclick}'">{buttonLabel}</button>
     """
     array = {
         "name": f"{buttonName}",
+        "code": html_code
+    }
+    return array
+
+def createCheckbox(id, name, label,  posx, posy, checked=False):
+    html_code = f"""
+<div class="form-check">
+   <input class="form-check-input" type="checkbox" value="" id="{id}" style="position: absolute; left: {posx}px; top: {posy}px;">
+   <label class="form-check-label" for="{id}" style="position: absolute; left: {posx}px; top: {posy}px;">
+    {label}
+   </label>
+</div>
+"""
+    array = {
+        "name": name,
         "code": html_code
     }
     return array
@@ -31,14 +46,19 @@ def makeText(text=None, posy=None, posx=None, link=None, underlined=False, bold=
     """
     return html_code
 
-def makeEntryBox(id, placeholder, actionFunction, posx, posy, buttonPosX, buttonPosY, buttonText):
+def makeEntryBox(id, placeholder, barText, classType, entryName, posx, posy):
     html_code = f"""
-    <form method="POST" action="{actionFunction}">
-        <input type="text" id="{id}" name="{id}" placeholder="{placeholder}" style="position: absolute; left: {posx}px; top: {posy}px;">
-        <input type="submit" value="{buttonText}" style="position: absolute; left: {buttonPosX}px; top: {buttonPosY}px;">
-    </form>
+<div style="position: absolute; left: {posx}px; top: {posy}px;" class="input-group flex-nowrap">
+  <span class="input-group-text" id="{id}">{barText}</span>
+  <input type="text" class="{classType}" placeholder="{placeholder}" aria-label="{placeholder}" aria-describedby="addon-wrapping">
+</div>
     """
-    return html_code
+    array = {
+        "name": f"{entryName}",
+        "code": html_code,
+    }
+
+    return array
 
 def createImage(fileName, posX, posY):
     html_code = f"""
@@ -46,26 +66,3 @@ def createImage(fileName, posX, posY):
     """
     return html_code
 
-def createTable():
-    ''
-
-def createDropdown(classType, dropArray):
-    ''
-
-def createScrollspy():
-    ''
-
-def createProgressBar():
-    ''
-
-def createSpinner():
-    ''
-
-def createToast():
-    ''
-    
-def createBadge():
-    ''
-
-def createModal():
-    ''
