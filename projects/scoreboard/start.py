30ba92a2bc6b4a0561513000ee80b987bf02a8a0
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-11-13 13:17:00 -0600

    done

--- projects/scoreboard/start.py
+++ projects/scoreboard/start.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from tkinter import messagebox
+
 def handelGraphics(timer, timerValue, possession, switch, team1, team2, homeTeam):
-    global root, colorLeft, colorRight, left, right, score_labelL, score_labelR, scoreL, scoreR
+    global root, colorLeft, colorRight, left, right, score_labelL, score_labelR, scoreL, scoreR, side
     scoreL = 0
     scoreR = 0
+    side = "left"
 
     root = tk.Tk()
     root.geometry('500x400')
@@ -21,23 +22,32 @@ def handelGraphics(timer, timerValue, possession, switch, team1, team2, homeTeam
             toast = tk.Label(root, text='Press f11 to toggle fullscreen', background='gray', foreground='white')
             toast.place(x=250, y=15, anchor='center')
             root.after(2000, lambda: toast.destroy())
-        
 
+    def update_scores(event):
+        global scoreL, scoreR, side
+
+        if event.keysym == 'q':
+            scoreL += 1
+        elif event.keysym == 'w' and scoreL > 0:
+            scoreL -= 1
+        elif event.keysym == 'o':
+            scoreR -= 1
+        elif event.keysym == 'p':
+            scoreR += 1
+        elif event.keysym == 'space':
+            scoreL, scoreR = scoreR, scoreL
 
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
 
-    print(f"Screen Width: {screen_width}")
-    print(f"Screen Height: {screen_height}")
-
-    # Bind F11 key to toggle fullscreen
     root.bind("<F11>", toggle_fullscreen)
+    root.bind("<Key>", update_scores)
 
     canvas = tk.Canvas(width=10000, height=10000, bg='black')
     canvas.pack()
 
     def graphics():
-        global colorLeft, colorRight, left, right, score_labelR, score_labelL
+        global colorLeft, colorRight, left, right, score_labelR, score_labelL, side
         if root.attributes('-fullscreen'):
             posxl = root.winfo_screenwidth() / 2 - 7.5
             posxr2 = root.winfo_screenwidth() - 15
@@ -66,12 +76,23 @@ def handelGraphics(timer, timerValue, possession, switch, team1, team2, homeTeam
             canvas.delete(right)
             canvas.delete(score_labelL)
             canvas.delete(score_labelR)
-        left = canvas.create_rectangle(10, posyt, posxl, posyb, fill=colorLeft)
-        right = canvas.create_rectangle(posxr1, posyt, posxr2, posyb, fill=colorRight)
-        score_labelL = canvas.create_text(posxScoreL, posyScoreL, text=str(scoreL), fill="white", font=("Helvetica", fontSize), anchor="center")
-        score_labelR = canvas.create_text(posxScoreR, posyScoreR, text=str(scoreR), fill="white", font=("Helvetica", fontSize), anchor="center")
-        root.after(100, graphics)
 
+        if side == "left":
+            colorLeft = "#ff0000"
+            colorRight = "#0000ff"
+            left = canvas.create_rectangle(10, posyt, posxl, posyb, fill=colorLeft)
+            right = canvas.create_rectangle(posxr1, posyt, posxr2, posyb, fill=colorRight)
+            score_labelL = canvas.create_text(posxScoreL, posyScoreL, text=str(scoreL), fill="white", font=("Helvetica", fontSize), anchor="center")
+            score_labelR = canvas.create_text(posxScoreR, posyScoreR, text=str(scoreR), fill="white", font=("Helvetica", fontSize), anchor="center")
+        else:
+            colorLeft = "#0000ff"
+            colorRight = "#ff0000"
+            left = canvas.create_rectangle(10, posyt, posxl, posyb, fill=colorLeft)
+            right = canvas.create_rectangle(posxr1, posyt, posxr2, posyb, fill=colorRight)
+            score_labelL = canvas.create_text(posxScoreL, posyScoreL, text=str(scoreR), fill="white", font=("Helvetica", fontSize), anchor="center")
+            score_labelR = canvas.create_text(posxScoreR, posyScoreR, text=str(scoreL), fill="white", font=("Helvetica", fontSize), anchor="center")
+
+        root.after(100, graphics)
 
     left = None
     right = None
@@ -85,6 +106,5 @@ def handelGraphics(timer, timerValue, possession, switch, team1, team2, homeTeam
 
     root.mainloop()
 
-
 if __name__ == "__main__":
-    handelGraphics(10, 15)
+    handelGraphics(10, 15, 'possession', 'switch', 'team1', 'team2', 'homeTeam')
