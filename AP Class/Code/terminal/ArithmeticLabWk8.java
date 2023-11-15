e61e8588014ab065f4cd9dd8d67e4b07cb2232ff
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-11-14 20:37:22 -0600

    arithmetic lab done

---
+++ AP Class/Code/terminal/ArithmeticLabWk8.java
@@ -0,0 +1,46 @@
+/** 
+Judah Starkenburg Nov 11 2023
+AP Computer Science A-A
+Week 8 Arithmetic Lab 
+**/
+
+import java.util.Scanner;
+
+public class ArithmeticLabWk8 {
+    private int num1;
+    private int num2;
+
+    private void performOperations() {
+        // Operations:
+        int sum = num1 + num2;
+        int difference = num1 - num2;
+        int product = num1 * num2;
+        double average = (double) sum / 2;
+        int distance = Math.abs(difference);
+        int max = Math.max(num1, num2);
+        int min = Math.min(num1, num2);
+
+        // Print statements
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+        System.out.println("Product: " + product);
+        System.out.println("Average: " + average);
+        System.out.println("Distance: " + distance);
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+
+    public static void main(String[] args) {
+        // Input 25 and 20 to get same results as lab example
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first integer: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second integer: ");
+        int num2 = scanner.nextInt();
+
+        ArithmeticLabWk8 arithmeticLab = new ArithmeticLabWk8();
+        arithmeticLab.num1 = num1;
+        arithmeticLab.num2 = num2;
+        arithmeticLab.performOperations();
+    }
+}
--- AP Class/Code/terminal/test.java
+++ AP Class/Code/terminal/test.java
@@ -2,17 +2,9 @@ public class test
 {
       public static void main(String[] args)
       {
-         // measurements in feet
-             int w = 10;
-             int L = 50;
-             int h = 25;
-             int nW = 2; // number of windows
-             int nD = 1; // number of doors
-             int sizeW = 5*5; // size windows (x,y)
-             int sizeD = 3*8; // size doors (x,y)
-             System.out.print("Your surface area is: ");
-             System.out.print((w * L * h * 6) - ((nW * sizeW) + (nD * sizeD)));
-             System.out.println("ft squared (I don't know how to add exponent number.)");
+            String s = "Hello";
+            // String t = "World";
+            System.out.print(s.substring(s.length() / 2, s.length()));
       }
 }
 
