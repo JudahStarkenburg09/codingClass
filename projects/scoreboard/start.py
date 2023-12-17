84361fc83cbf066099ff9dfe6ea97a10a10122a6
Judah Starkenburg <judahstarkenburg@gmail.com>
2023-12-17 06:41:27 -0600

    main.exe dome

--- projects/scoreboard/start.py
+++ projects/scoreboard/start.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from tkinter import messagebox
 from tkinter import font
 from PIL import Image, ImageTk, ImageFilter
+import keyboard
 
 class ScoreboardApp:
     def __init__(self, timer, timer_value_str, possession, switch, team1, team2, colored_sides, home_icon, away_icon, has_period):
@@ -13,8 +14,6 @@ class ScoreboardApp:
         self.away_icon_path = away_icon
         self.origTimer = timer_value_str
         self.timerBase = None
-        self.away_icon_image = None
-        self.home_icon_image = None
         self.scoreL = 0
         self.scoreR = 0
         self.periodText = None
@@ -43,7 +42,7 @@ Press arrow keys (< >) to change possession
 Press Enter to Reset Timer
 Press Ctrl + Enter to Reset Score, Possession, and Timer
 Press F1 to show Keybinds again / Open Live Window
-Use the UP and DOWn arrow to change the period (v, ^)"""
+Use the UP and DOWN arrow to change the period (v, ^)"""
         self.width = 500
         self.height = 400
         self.root = tk.Tk()
@@ -54,7 +53,7 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
 
     def initialize_gui(self):
         self.root.bind("<F11>", self.toggle_fullscreen)
-        self.root.bind("<Key>", self.update_scores)
+        keyboard.on_press(self.update_scores)
 
         self.canvas = tk.Canvas(width=10000, height=10000, bg='black')
         self.canvas.pack()
@@ -89,14 +88,14 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
             self.isFullscreen = False
 
     def update_scores(self, event):
-        # print(f"Keybind Detected: {event.keysym}")
-        if event.keysym == 'space':
+        print(f"Keybind Detected: {event.name}")
+        if event.name == 'space':
             if self.switch_possible:
                 if self.side == "left":
                     self.side = "right"
                 else:
                     self.side = "left"
-        elif event.keysym == "Return" and event.state == 0x4:
+        elif keyboard.is_pressed("ctrl+enter"):
             self.paused = True
             self.timer_value = self.origTimer
             self.scoreL = 0
@@ -104,47 +103,47 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
             self.side = "left"
             self.currentPos = "←"
             self.period = 1
-        elif event.keysym == "Return":
+            print("Control+Enter Pressed")
+        elif event.name == "enter":
             self.paused = True
             self.timer_value = self.origTimer
             self.currentPos = "→"
             self.side = "right"
-        elif event.keysym == 't':
+            print("Enter Pressed")
+        elif event.name == 't':
             if self.paused:
                 self.paused = False
-                print("Timer Unpaused")
             else:
                 self.paused = True
-                print("Timer Paused")
-        elif event.keysym == "F1":
+        elif event.name == "f1":
             self.show_keybinds()
-        elif event.keysym == 'Right':
+        elif event.name == 'right':
             self.currentPos = "→"
-        elif event.keysym == 'Left':
+        elif event.name == 'left':
             self.currentPos = "←"
-        elif event.keysym == 'q':
+        elif event.name == 'q':
             if self.side == "left":
                 self.scoreL += 1
             else:
                 self.scoreR += 1
-        elif event.keysym == 'w':
+        elif event.name == 'w':
             if self.side == "left":
                 self.scoreL -= 1
             else:
                 self.scoreR -= 1
-        elif event.keysym == 'o':
+        elif event.name == 'o':
             if self.side == "left":
                 self.scoreR -= 1
             else:
                 self.scoreL -= 1
-        elif event.keysym == 'p':
+        elif event.name == 'p':
             if self.side == "left":
                 self.scoreR += 1
             else:
                 self.scoreL += 1
-        elif event.keysym == 'Up':
+        elif event.name == 'up':
             self.period += 1
-        elif event.keysym == "Down":
+        elif event.name == "down":
             self.period -= 1
 
     def graphics(self):
@@ -158,6 +157,10 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
             self.WThirds = self.root.winfo_screenwidth() / 3
             self.WFifths = self.root.winfo_screenwidth() / 5
             self.fontMultiply = 3
+            self.tBase1 = self.LRHalf - 200
+            self.tBase2 = -10
+            self.tBase3 = self.LRHalf + 200
+            self.tBase4 = 200
         else:
             self.fullWidth = self.width
             self.fullHeight = self.height
@@ -168,8 +171,12 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
             self.WThirds = self.width / 3
             self.WFifths = self.width / 5
             self.fontMultiply = 1
+            self.tBase1 = self.LRHalf - 100
+            self.tBase2 = -10
+            self.tBase3 = self.LRHalf + 100
+            self.tBase4 = 100
 
-        print(f"W: {self.fullWidth}, H: {self.fullHeight}")
+        # print(f"W: {self.fullWidth}, H: {self.fullHeight}")
 
         if (self.score_labelL):
             self.canvas.delete(self.score_labelL)
@@ -187,12 +194,6 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
         if self.possessionPossible:
             self.posText = self.canvas.create_text((self.LRHalf), (0+self.fullHeight/2.5), text=f"{self.currentPos}", fill="white" ,anchor="center", font=font.Font(family='ds-digital', size=40*self.fontMultiply))
 
-        if self.home_icon_image:
-            self.canvas.delete(self.home_icon_image)
-        
-        if self.away_icon_image:
-            self.canvas.delete(self.away_icon_image)
-
         if self.period_possible: 
             if self.periodText:
                 self.canvas.delete(self.periodText)
@@ -201,8 +202,6 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
             self.periodLabel = self.canvas.create_text((self.LRHalf), (self.TBHalf+self.TBQuarter-(40*self.fontMultiply)+self.fullHeight/12), text=f"{self.period}", font=font.Font(family='ds-digital', size=50*self.fontMultiply if not self.isFullscreen else 40*self.fontMultiply), fill="yellow", anchor='center')
 
         if self.side == "left":
-            self.color_left = "#ff0000"
-            self.color_right = "#0000ff"
             self.leftName = self.canvas.create_text((self.WFifths), (self.TBHalf+self.fullHeight/10), anchor='center', text=f"{self.team1Name}", fill="white", font=font.Font(family='ds-digital', size=30*self.fontMultiply))
             self.rightName = self.canvas.create_text((self.fullWidth-self.WFifths), (self.TBHalf+self.fullHeight/10), anchor='center', text=f"{self.team2Name}", fill="white", font=font.Font(family='ds-digital', size=30*self.fontMultiply))
             self.score_labelL = self.canvas.create_text((self.WFifths), (self.fullHeight-self.TBQuarter+self.fullHeight/10), text=str(self.scoreL), fill="white", font=font.Font(family='ds-digital', size=(70*self.fontMultiply)), anchor="center")
@@ -225,41 +224,36 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
                 self.canvas.create_image((self.fullWidth-self.WFifths), (self.TBQuarter+self.fullHeight/12), anchor="center", image=tk_image2)
                 # Ensure that the image is not garbage collected by keeping a reference
                 self.canvas.imageTwo = tk_image2
-        # else:
-        #     self.color_left = "#0000ff"
-        #     self.color_right = "#ff0000"
-        #     self.leftName = self.canvas.create_text(self.team1XYSize[0], self.team1XYSize[1], anchor='center',text=f"{self.team2Name}", fill="white", font=font.Font(family='ds-digital', size=self.team1XYSize[2]))
-        #     self.rightName = self.canvas.create_text(self.team2XYSize[0], self.team1XYSize[1], anchor='center', text=f"{self.team1Name}", fill="white", font=font.Font(family='ds-digital', size=self.team1XYSize[2]))
-        #     if self.coloredSides:
-        #         self.left = self. canvas.create_rectangle(10, self.posyt+50, self.posxl, self.posyb, fill=self.color_left)
-        #         self.right = self.canvas.create_rectangle(self.posxr1, self.posyt+50, self.posxr2, self.posyb, fill=self.color_right)
-        #     self.score_labelL = self.canvas.create_text(self.posxScoreL-50, self.posyScoreL+50, text=str(self.scoreR), fill="white", font=font.Font(family='ds-digital', size=self.fontSize), anchor="center")
-        #     self.score_labelR = self.canvas.create_text(self.posxScoreR+50, self.posyScoreR+50, text=str(self.scoreL), fill="white", font=font.Font(family='ds-digital', size=self.fontSize), anchor="center")
-        #     if self.home_icon_path:
-        #         image1 = Image.open(self.home_icon_path)
-        #         image1 = image1.resize((self.imagePosRight[2], self.imagePosRight[2]), Image.LANCZOS)
-        #         # Convert the image to a Tkinter PhotoImage object
-        #         tk_image1 = ImageTk.PhotoImage(image1)
-        #         # Display the image on the canvas
-        #         self.canvas.create_image(self.imagePosRight[0], self.imagePosRight[1], anchor=tk.NW, image=tk_image1)
-        #         # Ensure that the image is not garbage collected by keeping a reference
-        #         self.canvas.imageOne = tk_image1
-        #     if self.away_icon_path:
-        #         image2 = Image.open(self.away_icon_path)
-        #         image2 = image2.resize((self.imagePosLeft[2], self.imagePosLeft[2]), Image.LANCZOS)
-        #         # Convert the image to a Tkinter PhotoImage object
-        #         tk_image2 = ImageTk.PhotoImage(image2)
-        #         # Display the image on the canvas
-        #         self.canvas.create_image(self.imagePosLeft[0], self.imagePosLeft[1], anchor=tk.NW, image=tk_image2)
-        #         # Ensure that the image is not garbage collected by keeping a reference
-        #         self.canvas.imageTwo = tk_image2
+        else:
+            self.leftName = self.canvas.create_text((self.WFifths), (self.TBHalf+self.fullHeight/10), anchor='center', text=f"{self.team2Name}", fill="white", font=font.Font(family='ds-digital', size=30*self.fontMultiply))
+            self.rightName = self.canvas.create_text((self.fullWidth-self.WFifths), (self.TBHalf+self.fullHeight/10), anchor='center', text=f"{self.team1Name}", fill="white", font=font.Font(family='ds-digital', size=30*self.fontMultiply))
+            self.score_labelL = self.canvas.create_text((self.WFifths), (self.fullHeight-self.TBQuarter+self.fullHeight/10), text=str(self.scoreR), fill="white", font=font.Font(family='ds-digital', size=(70*self.fontMultiply)), anchor="center")
+            self.score_labelR = self.canvas.create_text((self.fullWidth-self.WFifths), (self.fullHeight-self.TBQuarter+self.fullHeight/10), text=str(self.scoreL), fill="white", font=font.Font(family='ds-digital', size=(70*self.fontMultiply)), anchor="center")    
+            if self.away_icon_path:
+                image1 = Image.open(self.away_icon_path)
+                image1 = image1.resize((int(self.fullWidth/5), int(self.fullWidth/5)), Image.LANCZOS)
+                # Convert the image to a Tkinter PhotoImage object
+                tk_image1 = ImageTk.PhotoImage(image1)
+                # Display the image on the canvas
+                self.canvas.create_image((self.WFifths), (self.TBQuarter+self.fullHeight/12), anchor="center", image=tk_image1)
+                # Ensure that the image is not garbage collected by keeping a reference
+                self.canvas.imageOne = tk_image1
+            if self.home_icon_path:
+                image2 = Image.open(self.home_icon_path)
+                image2 = image2.resize((int(self.fullWidth/5), int(self.fullWidth/5)), Image.LANCZOS)
+                # Convert the image to a Tkinter PhotoImage object
+                tk_image2 = ImageTk.PhotoImage(image2)
+                # Display the image on the canvas
+                self.canvas.create_image((self.fullWidth-self.WFifths), (self.TBQuarter+self.fullHeight/12), anchor="center", image=tk_image2)
+                # Ensure that the image is not garbage collected by keeping a reference
+                self.canvas.imageTwo = tk_image2
 
 
-        # self.timerText = self.canvas.create_text(self.posxT, self.posyT, text=f"{self.timer_value}", fill="red", font=font.Font(family='ds-digital', size=(self.scoreFontSize-50)), anchor="center")
-        # if self.timer:
-        #     if self.timerBase:
-        #         self.canvas.delete(self.timerBase)
-        #     self.timerBase = self.canvas.create_rectangle(self.tBase1, self.tBase2, self.tBase3, self.tBase4, fill=None, outline='gray', width=2)
+        self.timerText = self.canvas.create_text(self.LRHalf, self.tBase4/2, text=f"{self.timer_value}", fill="red", font=font.Font(family='ds-digital', size=(self.fontMultiply*40)), anchor="center")
+        if self.timer:
+            if self.timerBase:
+                self.canvas.delete(self.timerBase)
+            self.timerBase = self.canvas.create_rectangle(self.tBase1, self.tBase2, self.tBase3, self.tBase4, fill=None, outline='gray', width=2)
 
         self.root.after(10, self.graphics)
 
@@ -291,13 +285,78 @@ Use the UP and DOWn arrow to change the period (v, ^)"""
         keybinds_window = tk.Toplevel(self.root)
         keybinds_window.title("Live Window")
 
+        # Create labels and buttons
         label = tk.Label(keybinds_window, text=self.keybinds, padx=10, pady=10)
         label.pack()
+        def buttonScoreboard(team=False, amount=False, pressed=False):
+            if team:
+                if team == "L":
+                    self.scoreL += amount
+                if team == "R":
+                    self.scoreR += amount
+            elif pressed:
+                if pressed == "timer":
+                    if self.paused:
+                        self.paused = False
+                    else:
+                        self.paused = True
+                elif pressed == "reset timer":
+                    self.timer_value = self.origTimer
+                    self.paused = True
+                elif pressed == "pos change":
+                    if self.currentPos == "←":
+                        self.currentPos = "→"
+                    elif self.currentPos == "→":
+                        self.currentPos = "←"
+                elif pressed == "switch":
+                    if self.side == "left":
+                        self.side = "right"
+                        print("Side is Right")
+                    else:
+                        self.side = "left"
+                        print("Side is Left")
+                elif pressed == "period":
+                    self.period += amount
 
-        ok_button = tk.Button(keybinds_window, text="OK", command=keybinds_window.destroy)
+
+        # Up arrow button for "q"
+        up_q_button = tk.Button(keybinds_window, text="↑", command=lambda: buttonScoreboard("L", 1))
+        up_q_button.pack(side=tk.LEFT, padx=10)
+
+        # Down arrow button for "w"
+        down_w_button = tk.Button(keybinds_window, text="↓", command=lambda: buttonScoreboard("L", -1))
+        down_w_button.pack(side=tk.LEFT, padx=10)
+
+        # Up arrow button for "p"
+        up_p_button = tk.Button(keybinds_window, text="↑", command=lambda: buttonScoreboard("R", 1))
+        up_p_button.pack(side=tk.RIGHT, padx=10)
+
+        # Down arrow button for "o"
+        down_o_button = tk.Button(keybinds_window, text="↓", command=lambda: buttonScoreboard("R", -1))
+        down_o_button.pack(side=tk.RIGHT, padx=10)
+
+        ok_button = tk.Button(keybinds_window, text="Exit", command=keybinds_window.destroy)
         ok_button.pack(pady=10)
+
+        timerUnpause = tk.Button(keybinds_window, text="Pause/Unpause Timer", command=lambda: buttonScoreboard(False, False, "timer"))
+        timerUnpause.pack(pady=5)
+
+        timerReset = tk.Button(keybinds_window, text="Reset Timer", command=lambda: buttonScoreboard(False,False,"reset timer"))
+        timerReset.pack(pady=5)
+
+        posChange = tk.Button(keybinds_window, text="Change Possession", command=lambda: buttonScoreboard(False,False,"pos change"))
+        posChange.pack(pady=5)
+
+        addPeriod = tk.Button(keybinds_window, text="Period +", command=lambda: buttonScoreboard(False,1,"period"))
+        addPeriod.pack(padx=10, side=tk.LEFT)
+
+        subPeriod = tk.Button(keybinds_window, text="Period -", command=lambda: buttonScoreboard(False,-1,"period"))
+        subPeriod.pack(padx=10, side=tk.RIGHT)
+
+        switchSides = tk.Button(keybinds_window, text="Switch Sides", command=lambda: buttonScoreboard(False,False,"switch"))
+        switchSides.pack(pady=5)
+    
         keybinds_window.bind("<F11>", self.toggle_fullscreen)
-        keybinds_window.bind("<Key>", self.update_scores)
 
 if __name__ == "__main__":
-    app = ScoreboardApp(True, '04:13', True, 'switch', 'Home', 'Away', False, '//vm-file/Student/jstark09/Desktop/codingClass/projects/scoreboard/NCASymbol copy.png', False, True)
+    app = ScoreboardApp(True, '04:13', True, 'switch', 'Home', 'Away', False, r"C:\Users\judah\OneDrive\Desktop\OtherFolders\Coding\codingClass\projects\scoreboard\NCASymbol.png", r"C:\Users\judah\OneDrive\Desktop\OtherFolders\Coding\codingClass\projects\scoreboard\ANS png.png", True)
