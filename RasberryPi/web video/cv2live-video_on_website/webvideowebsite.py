96494e7bb0142973ccb014a5688aa459f16e4f00
Judah Starkenburg <judahstarkenburg@gmail.com>
2024-01-16 21:09:42 -0600

    Cool cv2 internet features created

---
+++ RasberryPi/web video/cv2live-video_on_website/webvideowebsite.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, Response
+import cv2
+
+app = Flask(__name__)
+cap = cv2.VideoCapture(0)  # 0 represents the default camera (change if needed)
+
+def generate():
+    while True:
+        ret, frame = cap.read()
+        _, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- Web/Group Generator/main.py
+++ Web/Group Generator/main.py
@@ -233,4 +233,4 @@ def save():
 
 
 if __name__ == '__main__':
-    app.run(port=7824)
+    app.run(host='0.0.0.0', port=5000, debug=True)
